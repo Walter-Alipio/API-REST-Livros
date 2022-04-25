a874e814e5db23aed8c6184a9e507e7a1c077647
Walter <silvawalteralipio@gmail.com>
2022-04-25 10:29:47 -0300

    make it better

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import app from "./src/app.js";
 
-const port = process.env.port || process.env.PORT; //ou porta do ambiente de produção ou porta local
+const port = process.env.port || process.env.PORT; //porta do ambiente de produção ou porta local
 
 //executando o servidor
 app.listen(port, () => {
